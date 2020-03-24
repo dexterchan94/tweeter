649cc84321c9a7c36bdf13976412d6adb9c4b59f
Dexter Chan <dexterchann@gmail.com>
2020-03-24 22:35:46 +0000

    add tweet post time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,10 +3,45 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+const timeElapsed = function(tweetData) {
+  const time = (Date.now() - tweetData.created_at) / 1000;
+  if (time < 60) {
+    return `${time.toFixed(0)} seconds ago`;
+  } else if (time < 60 * 60) {
+    return `${(time / 60).toFixed(0)} minutes ago`;
+  } else if (time < 60 * 60 * 24) {
+    return `${(time / 60 / 60).toFixed(0)} hours ago`;
+  } else if (time < 60 * 60 * 24 * 365) {
+    return `${(time / 60 / 60 / 24).toFixed(0)} days ago`;
+  } else {
+    return `${(time / 60 / 60 / 24 / 365).toFixed(0)} years ago`;
+  }
+};
+
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
+}
+
+const createTweetElement = (tweetData) => {
+  const markup = `
+  <article class="tweet">
+  <header>
+  <div>
+  <img src="${tweetData.user.avatars}">
+  <span> ${tweetData.user.name}</span>
+  </div>
+  <span class="username">${tweetData.user.handle}</span>
+  </header>
+  <p>${escape(tweetData.content.text)}</p>
+  <footer>
+  <span>${timeElapsed(tweetData)}</span><span><i class="fas fa-flag"></i><i class="fas fa-dove"></i><i class="fas fa-thumbs-up"></i></span>
+  </footer>
+  </article>
+  `;
+  return markup;
 }
 
 const renderTweets = function(tweets) {
@@ -17,46 +52,25 @@ const renderTweets = function(tweets) {
   }
 }
 
-const createTweetElement = (tweetData) => {
+const loadTweets = () => {
+  console.log("Performing AJAX GET");
 
-  const markup = `
-  <article class="tweet">
-    <header>
-      <div>
-        <img src="${tweetData.user.avatars}">
-        <span> ${tweetData.user.name}</span>
-      </div>
-      <span class="username">${tweetData.user.handle}</span>
-    </header>
-    <p>${escape(tweetData.content.text)}</p>
-    <footer>
-      <span>${tweetData.created_at}</span><span><i class="fas fa-flag"></i><i class="fas fa-dove"></i><i class="fas fa-thumbs-up"></i></span>
-    </footer>
-  </article>
-  `;
-  return markup;
-}
+  $.ajax('/tweets', { 
+    method: 'GET',
+    success: (data) => {
+      renderTweets(data);
+    },
+    error: () => {
+      alert("error!");
+    }
+  });
+
+};
 
 
 $(document).ready(function() {
 
-  const loadTweets = () => {
-    console.log("Performing AJAX GET");
-
-    $.ajax('/tweets', { 
-      method: 'GET',
-      success: (data) => {
-        renderTweets(data);
-      },
-      error: () => {
-        alert("error!");
-      }
-    });
-
-  };
-
   loadTweets();
-
 
   $("#new-tweet-form").on('submit', function (event) {
     event.preventDefault();
